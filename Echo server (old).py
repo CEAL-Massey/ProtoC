a3195c03c6b8aa583c6233e4714f4846a692c6f2
sam-holm <samueldholm@gmail.com>
2020-08-26 09:20:53 +1200

    Adapted to use Pandas library to edit CSV

--- Echo server (old).py
+++ Echo server (old).py
@@ -23,9 +23,25 @@ while True:
             #database.insert_entry(struct) #Load entry into MySQL database
             readings = pd.read_csv('C:/Users/Sam Holm/Desktop/Quick Access Stuff Mk 4/Massey 2020/Capstone/WebsiteDjango/mysite/CEAL/templates/CEAL/readings.csv')
             if (readings.shape[0] >= 20):
-                readings.drop([0], inplace = True) #Keep the csv at approximately 20 entries
+                readings.drop([0], inplace = True)
             as_array = [struct.timestamp, struct.data]
             readings = readings.append(pd.Series(as_array, index=readings.columns), ignore_index = True)
             readings.to_csv(r'C:/Users/Sam Holm/Desktop/Quick Access Stuff Mk 4/Massey 2020/Capstone/WebsiteDjango/mysite/CEAL/templates/CEAL/readings.csv', index = False)
+            
+            # with open('C:/Users/Sam Holm/Desktop/Quick Access Stuff Mk 4/Massey 2020/Capstone/WebsiteDjango/mysite/CEAL/templates/CEAL/readings.csv', 'a', newline = '') as csv_file:
+            #     #check contents - is there 20 entries? if so, remove oldest and then add
+            #     #add new entry
+            #     csv_reader = csv.reader(csv_file, delimiter = ',')
+            #     data = list(csv_reader)
+            #     count = len(data)
+            #     if count >= 20:
+            #         data.remove(row[0])
+            #     end
+            #     csv_writer = csv.writer(csv_file, delimiter = ',')
+            #     as_array = [struct.timestamp, struct.data,',']
+            #     print(as_array)
+            #     csv_writer.writerow(as_array) #Drop the latest reading into the txt file        
+            # #if not data:
+            # #    break
             conn.sendall(data)
             s.close()
