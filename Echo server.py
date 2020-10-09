d2f3aeb352b09c0d5acf96cc1a9b1cff737b8db1
sam-holm <samueldholm@gmail.com>
2020-10-09 13:13:21 +1300

    Final Server edition

--- Echo server.py
+++ Echo server.py
@@ -6,10 +6,15 @@ import database
 import csv
 import pandas as pd
 
-HOST = '192.168.20.7'  
-PORT = 8886     # Port to listen on (non-privileged ports are > 1023)
+HOST = '192.168.1.3'  
+PORT = 8887     # Port to listen on (non-privileged ports are > 1023)
 
 struct = proto_struct_pb2.sens_dat()
+
+load = [0,0,0]
+flag = 0
+ovf = 0
+prev_array = [0,0]
 
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -22,10 +27,15 @@ while True:
             struct.ParseFromString(data) #De-serialize string into struct
             #database.insert_entry(struct) #Load entry into MySQL database
             readings = pd.read_csv('C:/Users/Sam Holm/Desktop/Quick Access Stuff Mk 4/Massey 2020/Capstone/WebsiteDjango/mysite/CEAL/templates/CEAL/readings.csv')
-            if (readings.shape[0] >= 20):
+            if (readings.shape[0] >= 50):
                 readings.drop([0], inplace = True) #Keep the csv at approximately 20 entries
             as_array = [struct.timestamp, struct.data]
-            readings = readings.append(pd.Series(as_array, index=readings.columns), ignore_index = True)
+            if ((prev_array != as_array) and (flag == 1) and (prev_array[1] != ovf)):
+                load = [prev_array[0], prev_array[1], as_array[1]]
+                readings = readings.append(pd.Series(load, index=readings.columns), ignore_index = True)
+                ovf = as_array[1]
+            prev_array = as_array
+            flag = 1
             readings.to_csv(r'C:/Users/Sam Holm/Desktop/Quick Access Stuff Mk 4/Massey 2020/Capstone/WebsiteDjango/mysite/CEAL/templates/CEAL/readings.csv', index = False)
             conn.sendall(data)
             s.close()
